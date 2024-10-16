7b3ac3c85544823e3029403426d54252aaf59345
Kefelyn <kefelyn.vitoria4@gmail.com>
2024-10-16 00:51:12 -0300

    DESAFIO FEITO

---
+++ index.js
@@ -0,0 +1,34 @@
+class heroi{
+    constructor(nome, idade, tipo){
+        this.nome = nome
+        this.idade = idade
+        this.tipo = tipo
+    }
+
+    atacar(){
+
+        let ataque = ""
+
+        if (this.tipo === "mago"){
+            ataque = "magia"
+        }
+    
+        else if (this.tipo === "guerreiro"){
+            ataque = "espada"
+        }
+    
+        else if (this.tipo === "monge"){
+            ataque = "artes marciais"
+        }
+    
+        else if (this.tipo === "ninja"){
+            ataque = "shuriken"
+        }
+
+        console.log("o " + this.tipo + " atacou usando " + ataque)
+    }
+}
+
+let heroiDev = new heroi("Kesty", 20, "mago")
+
+heroiDev.atacar()
